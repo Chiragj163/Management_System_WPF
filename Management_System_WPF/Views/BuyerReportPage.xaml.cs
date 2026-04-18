94b624eb35a52ecf1a2ea912f3147b8dabdeabee
Your Name <you@example.com>
2026-04-19 03:07:29 +0530

    Fix the scroll issue in buyer report page

--- Management_System_WPF/Views/BuyerReportPage.xaml.cs
+++ Management_System_WPF/Views/BuyerReportPage.xaml.cs
@@ -646,5 +646,12 @@ namespace Management_System_WPF.Views
                 _hostWindow = null;
             }
         }
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ScrollViewer scv = (ScrollViewer)sender;
+            scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
     }
+
 }
